e3bb82d8b810cdf5c5f1b68bb51d9a97e3cd4507
Yernar <ernar_9.6@bk.ru>
2022-12-12 13:55:49 +0600

    Yernar-Meetnet-last

--- src/views/pages/blackbox/main.js
+++ src/views/pages/blackbox/main.js
@@ -1,1 +1,3 @@
 console.log('\n>> Black Box landing page');
+
+
--- src/views/pages/meetnet/main.js
+++ src/views/pages/meetnet/main.js
@@ -1,1 +1,15 @@
-console.log('\n>> Blog page');
+console.log('\n>> Meetnet');
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 1,
+    spaceBetween: 30,
+    loop: true,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
+
